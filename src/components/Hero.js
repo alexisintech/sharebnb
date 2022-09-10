d10aa82f5d16a606b1b394bbb28967a1fd5ab68d
Alexis Aguilar <alexmerona@gmail.com>
2022-09-10 00:33:30 -0400

    fixed alt tag issues (a11y)

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,7 @@
 export default function Hero(){
   return(
     <section className="hero">
-      <img src="../imgs/photo-grid.png" className="hero--photo" />
+      <img src="../imgs/photo-grid.png" className="hero--photo" alt="grid of many randomized experience opportunities that could appear on sharebnb" />
       <h1 className="hero--header">Online Experiences</h1>
       <p className="hero--text">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
     </section>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Navbar(){
   return(
     <nav>
-      <img src="../imgs/airbnb-logo.png" className="nav--logo" />
+      <img src="../imgs/airbnb-logo.png" className="nav--logo" alt="airbnb logo" />
     </nav>
   )
 }
